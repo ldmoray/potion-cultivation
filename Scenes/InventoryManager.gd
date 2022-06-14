26797584ee7355f487cdc6bd35654529ac8e1660
Lenny Morayniss <ldmoray@gmail.com>
2022-06-13 20:45:02 -0400

    Dev Console and Font
    Adding a style font so the game looks a bit more cohesive

--- Scenes/InventoryManager.gd
+++ Scenes/InventoryManager.gd
@@ -5,6 +5,17 @@ signal inventory_changed
 var flowers: int = 0 setget set_flowers, get_flowers
 var coins: int = 0 setget set_coins, get_coins
 
+func _ready():
+	Console.add_command("setFlowers", self, "set_flowers")\
+		.set_description("Set flower count.")\
+		.add_argument("new_value", TYPE_INT)\
+		.register()
+	
+	Console.add_command("setCoins", self, "set_coins")\
+		.set_description("Set coin count.")\
+		.add_argument("new_value", TYPE_INT)\
+		.register()
+
 func set_flowers(new_value: int):
 	flowers = new_value
 	emit_signal("inventory_changed")
